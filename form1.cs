dc5ea5dcb769fa69caeafd3a77fc91d0cd945e79
Müge Değerliyurt <muge.degerliyurt@yandex.com>
2013-10-03 14:02:50 +0300

    Update form1.cs

--- form1.cs
+++ form1.cs
@@ -1,12 +1,12 @@
 public partial class Form1 : Form
     {
-        GMapOverlay _overlayOne;
+        private GMapOverlay _overlayOne;
         public static String LatLangValues { get; set; }
-        readonly BackgroundWorker _bw = new BackgroundWorker();
+        private readonly BackgroundWorker _bw = new BackgroundWorker();
+
         //Arduino seri portundan data çekmek için gerekli tanımlamalar.
-        readonly SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.None);
-
-
+        private readonly SerialPort _serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+        
         public Form1()
         {
             InitializeComponent();
@@ -16,56 +16,67 @@ public partial class Form1 : Form
         {
             _bw.DoWork += SerialPortReceive;
             _bw.RunWorkerAsync();
-            Console.Read();
+             Console.Read();
         }
 
-        private void SerialPortReceive(object sender, DoWorkEventArgs e)
+        private void SerialPortReceive(object sender,DoWorkEventArgs e)
         {
-            _serialPort.Open();
-
-            while (true)
-            {
-                // $GPGGA,hhmmss.dd,xxmm.dddd,<N|S>,yyymm.dddd,<E|W>,v,ss,d.d,h.h,M,g.g,M,a.a,xxxx*hh<CR><LF> formatında data Arduinodan karakter karakter gelecek.
+                _serialPort.Open();
                 
-                char[] charBufferValue = {};
-
-                int index = 0;
-                var charBuffer = new char[200];
-
                 while (true)
                 {
-                    string latValue = string.Empty;
-                    string langValue = string.Empty;
+                    // $GPGGA,hhmmss.dd,xxmm.dddd,<N|S>,yyymm.dddd,<E|W>,v,ss,d.d,h.h,M,g.g,M,a.a,xxxx*hh<CR><LF> formatında data Arduinodan karakter karakter gelecek.
 
-                    var receivedChar = (char)_serialPort.ReadChar();
-                    charBuffer[index] = receivedChar;
-                    index++;
-
-                   if (receivedChar == 0x10) // lf'in hex değerini koy
+                var index = 0;
+                var charBuffer = new char[100000];
+                    while (true)
                     {
-                        var s = new String(charBufferValue, 0, index);
-                        //virgüle göre split ettik gelen dataları, böylece her "," de array e bir eleman atıyoruz.
-                        String[] stringValues = s.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        var receivedChar = (char) _serialPort.ReadChar();
+                        charBuffer[index] = receivedChar;
+                        index++;
 
-                        //Arduino'dan gelen formatta lat ve lang indekslerinin alanları.
-                        string latValue = stringValues[2];
-                        string langValue = stringValues[4];
+                        if (receivedChar == 0x0A) // lf'in \n için hex değerini koy
+                        {
+                            var s = new String(charBuffer, 0, index);
+                            //virgüle göre split ettik gelen dataları, böylece her "," de array e bir eleman atıyoruz.
+                            String[] stringValues = s.Split(new[] {"LAT"}, StringSplitOptions.RemoveEmptyEntries);
 
-                        double latValueDouble = Convert.ToDouble(latValue);
-                        double langValueInteger = Convert.ToDouble(langValue);
+                            string mm = stringValues[stringValues.Length - 1].Replace("\r\n","");
 
-                        SetOverlay(latValueDouble, langValueInteger);
-                        index = 0;
+                            string[] value = mm.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            double latValueDouble = Convert.ToDouble(value[0].Replace("=", "").Replace("LON",""));
+                            double langValueDouble = Convert.ToDouble(value[1].Replace("=", "").Replace("LON", ""));
+
+                            SetOverlay(latValueDouble, langValueDouble);
+
+                            #region old code
+                            ////GPGGA lat,lang datasının geldiği sıralama Fastrax için
+                            //if (stringValues[0] == "$GPGGA")
+                            //{
+                            //    //Arduino'dan gelen formatta lat ve lang indekslerinin alanları.
+                            //    string latValue = stringValues[3];
+                            //    string langValue = stringValues[4];
+
+                            //    double latValueDouble = Convert.ToDouble(latValue);
+                            //    double langValueDouble = Convert.ToDouble(langValue);
+
+                            //    index = 0;
+
+                            //}
+                            #endregion
+
+                        } 
                     }
                 }
-            }
-        // ReSharper disable FunctionNeverReturns
+            
+            // ReSharper disable FunctionNeverReturns
         }
 
         public void SetOverlay(double serialDataLat,double serialDataLang)
         {
             gMapControl.Position = new PointLatLng(serialDataLat, serialDataLang);
-            gMapControl.MapProvider = GMapProviders.GoogleMap;
+            gMapControl.MapProvider = GMapProviders.BingMap;
             gMapControl.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
             //right click to drag map
             gMapControl.CanDragMap = true;
@@ -76,8 +87,16 @@ public partial class Form1 : Form
             gMapControl.Manager.Mode = AccessMode.ServerAndCache;
             //overlay adjust
             _overlayOne = new GMapOverlay(gMapControl, "OverlayOne");
+            
             //marker adjust
-            _overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMapMarkerCross(new PointLatLng(serialDataLat, serialDataLang)));
+            //_overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMapMarkerCross(new PointLatLng(serialDataLat, serialDataLang)));
+
+            //TODO: To change overlay marker image
+
+            var point  = new PointLatLng(serialDataLat,serialDataLang);
+            Image image = Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)+ @"\icon.ico");
+            _overlayOne.Markers.Add(new GmapMarker(point,image));
+
             //pinning on map
             gMapControl.Overlays.Add(_overlayOne);
         }
