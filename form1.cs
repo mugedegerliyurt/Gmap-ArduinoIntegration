61ba8bb997a5762bc627020959894759c6b41080
Müge Değerliyurt <muge.degerliyurt@yandex.com>
2013-10-04 09:28:26 +0300

    Last version

--- form1.cs
+++ form1.cs
@@ -17,43 +17,43 @@ public partial class Form1 : Form
             _bw.DoWork += SerialPortReceive;
             _bw.RunWorkerAsync();
              Console.Read();
+
+            //haritayı cachele
+            gMapControl.Manager.Mode = AccessMode.ServerAndCache;
+            gMapControl.MinZoom = 3;
+            gMapControl.MaxZoom = 18;
+            gMapControl.Zoom = 15;
         }
 
         private void SerialPortReceive(object sender,DoWorkEventArgs e)
         {
                 _serialPort.Open();
-                
                 while (true)
                 {
                     // $GPGGA,hhmmss.dd,xxmm.dddd,<N|S>,yyymm.dddd,<E|W>,v,ss,d.d,h.h,M,g.g,M,a.a,xxxx*hh<CR><LF> formatında data Arduinodan karakter karakter gelecek.
 
-                var index = 0;
-                var charBuffer = new char[100000];
+                    var index = 0;
+                    var charBuffer = new char[100000];
                     while (true)
                     {
                         var receivedChar = (char) _serialPort.ReadChar();
                         charBuffer[index] = receivedChar;
                         index++;
-
-                        if (receivedChar == 0x0A) // lf'in \n için hex değerini koy
+                        // lf'in \n için hex değerini koy
+                        if (receivedChar == 0x0A) 
                         {
                             var s = new String(charBuffer, 0, index);
                             //virgüle göre split ettik gelen dataları, böylece her "," de array e bir eleman atıyoruz.
-                            String[] stringValues = s.Split(new[] {"LAT"}, StringSplitOptions.RemoveEmptyEntries);
-
-                            string mm = stringValues[stringValues.Length - 1].Replace("\r\n","");
-
-                            string[] value = mm.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                            double latValueDouble = Convert.ToDouble(value[0].Replace("=", "").Replace("LON",""));
-                            double langValueDouble = Convert.ToDouble(value[1].Replace("=", "").Replace("LON", ""));
+                            String[] stringValues = s.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
                             
-                            Invoke((MethodInvoker)(() =>
+                            double latValueDouble = Convert.ToDouble(stringValues[0]);
+                            double longValueDouble = Convert.ToDouble(stringValues[1]);
+
+                            if (!IsDisposed)
                             {
-                                SetOverlay(latValueDouble, langValueDouble);
-                            }));
+                                Invoke((MethodInvoker)(() => SetOverlay(latValueDouble, longValueDouble)));
+                            }
                             
-
                             #region old code
                             ////GPGGA lat,lang datasının geldiği sıralama Fastrax için
                             //if (stringValues[0] == "$GPGGA")
@@ -69,39 +69,31 @@ public partial class Form1 : Form
 
                             //}
                             #endregion
-
                         } 
                     }
                 }
-            
             // ReSharper disable FunctionNeverReturns
         }
 
-        public void SetOverlay(double serialDataLat,double serialDataLang)
+        public void SetOverlay(double serialDataLat,double serialDataLong)//bunu long'a refactor et
         {
-            gMapControl.Position = new PointLatLng(serialDataLat, serialDataLang);
-            gMapControl.MapProvider = GMapProviders.BingMap;
-            gMapControl.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
-            //right click to drag map
-            gMapControl.CanDragMap = true;
-            gMapControl.MinZoom = 3;
-            gMapControl.MaxZoom = 30;
-            gMapControl.Zoom = 5;
+            if (gMapControl.Overlays.Count >= 1) //yanıp sönme olmasın bence olsun ya sanki hareket ediyormuş gbi oluyordu :d havalı :D
+                gMapControl.Overlays.RemoveAt(0);
 
-            gMapControl.Manager.Mode = AccessMode.ServerAndCache;
+            gMapControl.Position = new PointLatLng(serialDataLat, serialDataLong);//aynısından burada da var? görmedim onu. Bu centerlıyor.
+            gMapControl.MapProvider = GMapProviders.BingMap;
+            //gMapControl.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            
+            
+            
+            //gMapControl.Manager.Mode = AccessMode.ServerAndCache;
             //overlay adjust
             _overlayOne = new GMapOverlay(gMapControl, "OverlayOne");
             
-            //marker adjust
-            //_overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMapMarkerCross(new PointLatLng(serialDataLat, serialDataLang)));
-
-            //TODO: To change overlay marker image
-
-            var point  = new PointLatLng(serialDataLat,serialDataLang);
+            var point  = new PointLatLng(serialDataLat,serialDataLong);
             Image image = Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)+ @"\icon.ico");
             _overlayOne.Markers.Add(new GmapMarker(point,image));
-
-            //pinning on map
+           
             gMapControl.Overlays.Add(_overlayOne);
         }
     }
